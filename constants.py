9885df8cf1ef1959386893c04f3cf19997c81093
Matthew Riche <matthew.riche@gmail.com>
2021-06-06 19:59:46 -0400

    Bake over time feature ready

--- constants.py
+++ constants.py
@@ -10,30 +10,49 @@ Python constants idiom.  These are the node names common to Shaper Rigs biped pr
 
 # SSC default bone names
 INTERNAL_DEF_FK_JNTS = (
-    { 'shoulder':'armUprFK_drv', 
+    { 
+    'shoulder':'armUprFK_drv', 
     'elbow':'armLwrFK_drv', 
-    'wrist':'armWristFK_drv' 
+    'wrist':'armWristFK_drv',
+    'hip':'legUprFK_drv',
+    'knee':'legLwrFK_drv',
+    'ankle':'legAnkleFK_drv'
     }
     )
 
 INTERNAL_DEF_IK_JNTS = (
-    {'shoulder':'armUprIK_drv', 
+    {
+    'shoulder':'armUprIK_drv', 
     'elbow':'armLwrIK_drv', 
-    'wrist':'armWristIK_drv' }
+    'wrist':'armWristIK_drv',
+    'hip':'legUprIK_drv',
+    'knee':'legLwrIK_drv',
+    'ankle':'legAnkleIK_drv'
+    }
     )
 
 # SSC default controls names
 INTERNAL_DEF_FK_CTRLS = (
-    { 'shoulder':'armUprFK_Ctrl', 
-        'elbow':'armLwrFK_Ctrl', 
-        'wrist':'armWristFK_Ctrl' }
-        )
+    { 
+    'shoulder':'armUprFK_Ctrl', 
+    'elbow':'armLwrFK_Ctrl', 
+    'wrist':'armWristFK_Ctrl',
+    'hip':'legUprFK_Ctrl',
+    'knee':'legLwrFK_Ctrl',
+    'ankle':'legAnkleFK_Ctrl'
+    }
+    )
 
 INTERNAL_DEF_IK_CTRLS = (
-    { 'shoulder':'armUprIK_Ctrl', 
-        'elbow':'ArmPV_Ctrl', 
-        'wrist':'armWristIK_Ctrl', 
-        'pv_offset':'ArmPV_nOffset' }
-        )
+    { 
+    'shoulder':'armUprIK_Ctrl', 
+    'elbow':'ArmPV_Ctrl', 
+    'wrist':'armWristIK_Ctrl', 
+    'pv_offset_elbow':'ArmPV_nOffset',
+    'hip':'legUprIK_Ctrl',
+    'knee':'LegPV_Ctrl',
+    'ankle':'legAnkleIK_Ctrl'
+    }
+    )
 
 INTERNAL_SIDE_TOKENS = { 'left':'L_', 'right':'R_' }
--- fkik.py
+++ fkik.py
@@ -6,11 +6,9 @@ import coord_math as m
 import pymel.core as pm
 import numpy as np
 import constants as cons
-import copy
 
 
-def fk_to_ik(side=None, ik_bones_dict=None, 
-    fk_ctrls_dict=None):
+def fk_to_ik(side=None, limb=None, ik_bones_dict=None, fk_ctrls_dict=None, key=False):
     '''
     Match fk controls to ik, but executing match xforms of the controllers to the bones.  Generic
     and ready to receive rig info for any rig with a 'copied arm' set up for fk/ik.  Assuming that
@@ -19,6 +17,15 @@ def fk_to_ik(side=None, ik_bones_dict=None,
     ik_bones_dict - dictionary containing keys 'shoulder' and 'elbow', etc, listing the ik bones.
     fk_ctrls_dict - dictionary containing keys 'shoulder' and 'elbow' etc, listing fk controls.
     '''
+
+    # Based on the limb string incoming, the following keys will be used in the dictionary.
+    if(limb == 'leg'):
+        targets_list = ['hip', 'knee', 'ankle']
+    elif(limb == 'arm'):
+        targets_list = ['shoulder', 'elbow', 'wrist']
+    else:
+        pm.warning("Must specific a limb with either 'leg' or 'arm'.")
+        return
 
     # Assign defaults like so to dodge the mutable default argument issue:
     if(ik_bones_dict == None):
@@ -38,7 +45,6 @@ def fk_to_ik(side=None, ik_bones_dict=None,
         pm.error("Given side-flag string was weird.  Try 'r' or 'l'.")
         return
 
-
     # Append the side token to all strings.
     for bone in ik_bones_dict:
         ik_bones_dict[bone] = (side_token + ik_bones_dict[bone])
@@ -49,7 +55,6 @@ def fk_to_ik(side=None, ik_bones_dict=None,
     print ("Side tokens added, ctrl targets are:\n {}".format(ik_bones_dict))
 
     # Iterate through the list of key names, perform the xform matching.
-    targets_list = ['shoulder', 'elbow', 'wrist']
     for target_key in targets_list:
         print("Matching transforms of {} to {}...".format(
             fk_ctrls_dict[target_key], ik_bones_dict[target_key]
@@ -59,12 +64,19 @@ def fk_to_ik(side=None, ik_bones_dict=None,
         copy_node = pm.PyNode(ik_bones_dict[target_key])
         pm.matchTransform(target_node, copy_node, pos=True, piv=True)
 
+    # Put keyframes on all the FK controls if key is true.
+    if(key==True):
+        for ctrl in fk_ctrls_dict:
+            pm.setKeyframe(fk_ctrls_dict[ctrl], at=['translate', 'rotate'])
+            print ("Keying {}".format(fk_ctrls_dict[ctrl]))
+
+
     print ("Done.")
 
     return
 
     
-def ik_to_fk(side=None, fk_bones_dict=None, ik_ctrls_dict=None):
+def ik_to_fk(side=None, limb=None, fk_bones_dict=None, ik_ctrls_dict=None, key=False):
     '''
     Move IK controls to match FK position.
     Ready to receive rig info for any rig with a 'copied arm' set up for fk/ik.  Assuming that the 
@@ -84,6 +96,15 @@ def ik_to_fk(side=None, fk_bones_dict=None, ik_ctrls_dict=None):
     if(ik_ctrls_dict == None):
         ik_ctrls_dict = cons.INTERNAL_DEF_IK_CTRLS.copy()
 
+    # Based on the limb string incoming, the following keys will be used in the dictionary.
+    if(limb == 'leg'):
+        targets_list = ['hip', 'knee', 'ankle']
+    elif(limb == 'arm'):
+        targets_list = ['shoulder', 'elbow', 'wrist']
+    else:
+        pm.warning("Must specific a limb with either 'leg' or 'arm'.")
+        return
+
     # Append the side flags
     if(side == None):      
         pm.error("Specify a side flag (Example fk_to_ik(side='l') )")
@@ -99,7 +120,6 @@ def ik_to_fk(side=None, fk_bones_dict=None, ik_ctrls_dict=None):
 
     print("request side token isn {}".format(side_token))
 
-
     # Append the side token to all strings.
     for bone in fk_bones_dict:
         fk_bones_dict[bone] = (side_token + fk_bones_dict[bone])
@@ -108,27 +128,25 @@ def ik_to_fk(side=None, fk_bones_dict=None, ik_ctrls_dict=None):
         ik_ctrls_dict[ctrl] = (side_token + ik_ctrls_dict[ctrl])
     print ("Side tokens added, ctrl targets are:\n {}".format(ik_ctrls_dict))
 
-
-
     # Step one, match ik shoulder 1:1
-    shoulder_target = pm.PyNode(fk_bones_dict['shoulder'])
-    shoulder_ctrl = pm.PyNode(ik_ctrls_dict['shoulder'])
-    pm.matchTransform(shoulder_ctrl, shoulder_target, pos=True, piv=True)
+    topmost_target = pm.PyNode(fk_bones_dict[targets_list[0]])
+    topmost_ctrl = pm.PyNode(ik_ctrls_dict[targets_list[0]])
+    pm.matchTransform(topmost_ctrl, topmost_target, pos=True, piv=True)
 
     # Step two, match ik wrist 1:1
-    wrist_target = pm.PyNode(fk_bones_dict['wrist'])
-    wrist_ctrl = pm.PyNode(ik_ctrls_dict['wrist'])
+    wrist_target = pm.PyNode(fk_bones_dict[targets_list[2]])
+    wrist_ctrl = pm.PyNode(ik_ctrls_dict[targets_list[2]])
     pm.matchTransform(wrist_ctrl, wrist_target, pos=True, rot=True, piv=True)
 
     # Step three, match the PV control 1:1 to the elbow
-    elbow_target = pm.PyNode(fk_bones_dict['elbow'])
-    elbow_ctrl = pm.PyNode(ik_ctrls_dict['elbow'])
+    elbow_target = pm.PyNode(fk_bones_dict[targets_list[1]])
+    elbow_ctrl = pm.PyNode(ik_ctrls_dict[targets_list[1]])
     pm.matchTransform(elbow_ctrl, elbow_target, pos=True) # Ignore the piv flag, maybe locked.
 
     # Step three-- extend the vectors where one is a ray from the hip to the knee, and the other is
     # the ankle to the knee.  This establishes the plane whereon the PV can live.
     vector_a = m.get_vector(
-        point_a=pm.xform(shoulder_target, q=True, t=True, ws=True),
+        point_a=pm.xform(topmost_target, q=True, t=True, ws=True),
         point_b=pm.xform(elbow_target, q=True, t=True, ws=True)
         )
     vector_b = m.get_vector(
@@ -149,4 +167,75 @@ def ik_to_fk(side=None, fk_bones_dict=None, ik_ctrls_dict=None):
     # Move the middle control "outward" along the out_vector.
     pm.xform(elbow_ctrl, t=(final_pos[0], final_pos[1], final_pos[2]), ws=True)
 
+    # Put keyframes on all the IK controls if key is true.
+    if(key==True):
+        for ctrl in ik_ctrls_dict:
+            pm.setKeyframe(ik_ctrls_dict[ctrl], at=['translate', 'rotate'])
+            print ("Keying {}".format(ik_ctrls_dict[ctrl]))
+
     print("Done.")
+
+
+def bake_ik_to_fk(side=None, limb=None):
+    '''
+    bake_ik_to_fk
+
+    Matches the ik position to the fk animation, keying it over the selected frames.
+
+    usage:
+    bake_ik_to_fk
+    '''
+
+    import maya.mel
+    PlayBackSlider = maya.mel.eval('$tmpVar=$gPlayBackSlider')
+    frame_range = pm.timeControl(PlayBackSlider, q=True, ra=True)
+
+    # Move the time slider to the beginning of the selected range.
+    pm.currentTime(frame_range[0], edit=True)
+    
+    # Iterate through the frame range selected.
+    while(pm.currentTime(q=True) < frame_range[1]):
+
+        # Bake the ik controllers to the position the fk controls are on this frame:
+        ik_to_fk(side=side, limb=limb, key=True)
+        next_frame = (pm.currentTime(q=True) + 1)
+        pm.currentTime(next_frame, edit=True)
+        pm.refresh(cv=True)
+
+    print ("Done.")
+
+
+def bake_fk_to_ik(side=None, limb=None):
+    '''
+    bake_fk_to_ik
+
+    Matches the fk position to the ik animation, keying it over the selected frames.
+
+    usage:
+    bake_fk_to_ik(side=string(token), limb=string(token))
+    Use with a frame range selected.
+    '''
+
+    import maya.mel
+    PlayBackSlider = maya.mel.eval('$tmpVar=$gPlayBackSlider')
+    frame_range = pm.timeControl(PlayBackSlider, q=True, ra=True)
+
+    # Move the time slider to the beginning of the selected range.
+    pm.currentTime(frame_range[0], edit=True)
+    
+    # Iterate through the frame range selected.
+    while(pm.currentTime(q=True) < frame_range[1]):
+
+        # Bake the fk controllers to the position the fk controls are on this frame:
+        fk_to_ik(side=side, limb=limb, key=True)
+        next_frame = (pm.currentTime(q=True) + 1)
+        pm.currentTime(next_frame, edit=True)
+        pm.refresh(cv=True)
+
+    print ("Done.")
+
+
+
+
+
+
