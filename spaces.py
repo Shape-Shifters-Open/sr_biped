65265c19f8be97f44d7ebdd8b67d41a5cfec76b6
Matthew Riche <matthew.riche@gmail.com>
2021-07-23 14:32:57 -0400

    Missing init entries fixed.  Spaces definitions now deal with rotation

--- spaces.py
+++ spaces.py
@@ -20,18 +20,19 @@ def get_world_space(node):
     '''
 
     pos = dt.Vector(pm.xform(node, q=True, t=True, ws=True))
+    rot = dt.Vector(pm.xform(node, q=True, ro=True, ws=True))
+    return [pos,rot]
 
-    return pos
 
-
-def match_and_key(node, pos):
+def match_and_key(node, pos, rot):
     '''
     Given a node and a vector to match to, match the node to the vector and key it.
     
     node - A transform node.
     '''
 
-    pm.match(node, ws=True, t=pos)
+    pm.xform(node, ws=True, t=pos)
+    pm.xform(node, ws=True, ro=rot)
     pm.setKeyframe(node, at=['translate', 'rotate'])
 
     print("{} keyed to {} in worldspace.".format(node.name(), pos))
